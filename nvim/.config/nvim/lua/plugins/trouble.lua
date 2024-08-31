f922d9f30c6a8e0c7e07f5e8b973426aa791887b
Frederic Robert <frederic.robert.axa@gmail.com>
2024-08-31 22:31:20 +0200

    Initial commit

---
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,15 @@
+return {
+    "folke/trouble.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>tt",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+    },
+}
