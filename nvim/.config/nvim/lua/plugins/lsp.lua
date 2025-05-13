4084719940ea50aff95db266889ecad8a39c3033
Frédéric Robert <frederic.robert.axa@gmail.com>
2025-05-13 20:23:39 +0200

    Update lsp config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -13,7 +13,12 @@ return {
         "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-        require("mason").setup()
+        require("mason").setup {
+            ui = {
+                border = "single",
+            }
+        }
+
         local mason_lspconfig = require("mason-lspconfig")
         mason_lspconfig.setup({
             automatic_installation = true,
@@ -21,12 +26,8 @@ return {
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-        mason_lspconfig.setup_handlers({
-            function(server_name)
-                require("lspconfig")[server_name].setup({
-                    capabilities = capabilities,
-                })
-            end,
+        require("lspconfig").clangd.setup({
+            cmd = { "/usr/bin/clangd" },  
         })
 
         -- Global mappings
